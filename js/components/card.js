60e886cb3888abe6df3f971431d2197555e21b55
blabb <blabb.a@ya.ru>
2026-01-18 15:13:17 +0300

    correct questions and popup

--- js/components/card.js
+++ js/components/card.js
@@ -20,9 +20,27 @@ export class QuizCardGenerator {
 
         titleCard.textContent = title;
         descCard.textContent = description;
-        countCard.textContent = `${questionCount} вопросов`;
+        countCard.textContent = `${questionCount} ${getQuestionWord(questionCount)}`;
         button.href = `${BASE_URL}quiz.html?id=${encodeURIComponent(id)}`;
 
         return fragment;
     }
+}
+
+function getQuestionWord(count) {
+    if (count % 100 >= 11 && count % 100 <= 14) {
+        return 'вопросов';
+    }
+
+    const lastDigit = count % 10;
+
+    if (lastDigit === 1) {
+        return 'вопрос';
+    }
+
+    if (lastDigit >= 2 && lastDigit <= 4) {
+        return 'вопроса';
+    }
+
+    return 'вопросов';
 }
--- js/pages/main.js
+++ js/pages/main.js
@@ -5,13 +5,21 @@ import { addQuiz } from "../utils/storage";
 import { Header } from "../components/header";
 
 const form = document.forms['json-block'];
-const buttonSubmit = document.querySelector('.generator-submit');
 const textarea = document.querySelector('.generator-input');
+const popup = document.querySelector('.popup');
+const popupDescription = document.querySelector('.popup__description');
+const popupButton = document.querySelector('.popup__button');
 
 const header = new Header();
 const quizGenerator = new QuizGenerator(ValidateQuizJson, addQuiz);
 
 header.setButtonItems([{text: "Посмотреть сохранённые квизы", href: `${BASE_URL}quizzes.html`, variant: "secondary"}])
+
+const popupCloseHandler = () => {
+    popup.classList.remove("popup--open");
+
+    popupButton.removeEventListener("click", popupCloseHandler);
+};
 
 const inputJSONHandler = async (event) => {
     event.preventDefault();
@@ -19,7 +27,12 @@ const inputJSONHandler = async (event) => {
     const response = await quizGenerator.setDataQuiz(textarea.value);
 
     if (!response.ok) {
+        popup.classList.add("popup--open");
+        popupButton.addEventListener("click", popupCloseHandler);
+
         textarea.classList.add("generator-input-error");
+        popupDescription.textContent = response.message;
+
         console.error(response);
     } else {
         window.location.href = `${BASE_URL}quizzes.html`;
